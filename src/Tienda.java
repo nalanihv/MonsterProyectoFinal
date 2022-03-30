2cf6fea531f820eea45db8e035b4b931c1b03526
Nalanihv <nalanihevi@gmail.com>
2022-03-30 12:00:25 -0600

    comienzo de la calse tienda

---
+++ src/Tienda.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Tienda {
+    /*atributos:
+   nombre- string
+    objetos disponibles -arrays<objetos>
+
+    metodos:
+    comprar
+    vender
+     */
+
+    private String nombre;
+    private ArrayList<Objeto>objetosDisponibles;
+
+    public boolean usuarioComprar(double dinero,int cantidad,int indiceObjeto){
+        /* validad que haya suficientes objetos del tipo requerido :
+        cantidad y existe en el arreglo
+        si si
+        if si(validar el dinero suficiente){
+        le vendo
+        }else{"no le alcanza"}
+
+        else() //exepcion de que no hay la cantidad que  pide
+        se repite la pregunta
+
+
+         */return false;
+    }
+    public boolean usuarioVender(Objeto objeto, int cantidad){
+        /*inverso de comprar
+        dinero infinito
+        recibir objetos y dar el dinero
+         */
+
+        return false;
+    }
+
+}
