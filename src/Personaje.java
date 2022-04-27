f54ea21dc78c74eb127697dd2cd387bdf0707a5f
Nalanihv <nalanihevi@gmail.com>
2022-04-27 11:43:31 -0500

    estructura de clases. 26.04.2022

--- src/Personaje.java
+++ src/Personaje.java
@@ -2,7 +2,29 @@ public class Personaje {
     protected String nombre;
     protected int nivel;
     protected char genero;
-    //getter, setter y contructor falta
+
+    public String getNombre() {
+        return nombre;}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;}
+
+    public int getNivel() {
+        return nivel;}
+    public void setNivel(int nivel) {
+        this.nivel = nivel;}
+
+    public char getGenero() {
+        return genero;}
+    public void setGenero(char genero) {
+        this.genero = genero;}
+
+    public Personaje(String nombre, int nivel, char genero) {
+        this.nombre = nombre;
+        this.nivel = nivel;
+        this.genero = genero;
+    }
+
+//getter, setter y contructor falta
     //nombre - string protected
     //nivel- int protected
     //genero- char protected
