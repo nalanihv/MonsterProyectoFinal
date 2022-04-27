803220b025431051008cea8e8d773fb50c16713b
nalanihv <100865881+nalanihv@users.noreply.github.com>
2022-04-26 21:40:16 -0500

    Update Personaje.java

--- src/Personaje.java
+++ src/Personaje.java
@@ -1,4 +1,8 @@
 public class Personaje {
+    protected String nombre;
+    protected int nivel;
+    protected char genero;
+    //getter, setter y contructor falta
     //nombre - string protected
     //nivel- int protected
     //genero- char protected
